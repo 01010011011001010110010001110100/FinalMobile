0af7ddfba9cc302c86d9bec398b8a0b3d04444ca
Sedtt <moisesgironarias@gmail.com>
2025-04-15 23:38:12 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Civil Defense',
+  webDir: 'www'
+};
+
+export default config;
